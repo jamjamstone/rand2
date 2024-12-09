c345ab902c499d6eb5a4b5b86fe7efdbe5342ba3
jamjamstone <junghyun4545@naver.com>
2024-12-09 09:39:55 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace rand2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            while (true) {
+                //Console.WriteLine(rand.Next());
+                //Console.WriteLine(rand.Next(0,10));
+                //Console.WriteLine(rand.NextDouble());
+                float myNum=(float)rand.Next(0,100)/100;// 소수점 랜덤 만들기
+                Console.WriteLine(myNum);
+            }
+        }
+    }
+}
